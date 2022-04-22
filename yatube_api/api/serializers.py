55e4ffa4e4c72fc0857b71df8d654aaa794166e1
Александр Вязников <alexnder.vyaznikov@yandex.ru>
2022-04-22 20:10:36 +1000

    Первый вариант кода

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 
 
-from posts.models import Comment, Post
+from posts.models import Comment, Post, Group, Follow, User
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -13,6 +13,13 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
 
 
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = ('id', 'title', 'slug', 'description')
+
+
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
         read_only=True, slug_field='username'
@@ -21,3 +28,27 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
         model = Comment
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+    following = serializers.SlugRelatedField(
+        slug_field='username', queryset=User.objects.all()
+    )
+
+    def validate(self, data):
+        if Follow.objects.filter(
+            user=self.context.get('request').user, following=data['following']
+        ).exists():
+            raise serializers.ValidationError(
+                'Нельзя подписаться на одного пользователя дважды'
+            )
+        if self.context['request'].user == data['following']:
+            raise serializers.ValidationError('Нельзя подписываться на себя')
+        return data
+
+    class Meta:
+        fields = ('user', 'following')
+        model = Follow
